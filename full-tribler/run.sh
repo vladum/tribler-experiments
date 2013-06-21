49cb09fa6893292b6ab825df9bf5ebe978c22d0a
Vlad Dumitrescu <vlad@dumitrescu.ro>
2013-06-21 12:15:23 +0200

    print unique

--- full-tribler/run.sh
+++ full-tribler/run.sh
@@ -8,8 +8,11 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/libs/lib:$HOME/libs/lib64
 export TRIBLERPATH=../../tribler
 
 UNIQUE=`date +%s | sha256sum | base64 | head -c 32`
+
+echo "UNIQUE is: $UNIQUE"
+
 prun -v -o peer node.py 4 $UNIQUE | tee hosts
 
-echo 'Done.'
+echo "Done."
 HOSTS=`cat hosts | head -1`
 echo $HOSTS
