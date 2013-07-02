aba0db799ff3439bc7c5f48315ece30318eccc19
Vlad Dumitrescu <vlad@dumitrescu.ro>
2013-07-02 18:44:21 +0200

    node using config server

--- full-tribler-gumby/node.py
+++ full-tribler-gumby/node.py
@@ -2,7 +2,24 @@
 
 import sys
 import os
+from twisted.internet import reactor
 
-if __name__ == '__main__':
-    print 'I am node', sys.argv[1]
-    print os.environ
+from gumby.config import ConfigClientFactory, get_config_server_endpoint
+
+def start_experiment(config):
+    print "\n\nConfiguration received. Starting experiment."
+    print config
+
+def main():
+    factory = ConfigClientFactory()
+    factory.onConfigReceived(start_experiment)
+
+    ip, port = get_config_server_endpoint()
+    reactor.connectTCP(ip, port, factory)
+
+    # run experiment for a few seconds
+    reactor.callLater(4, reactor.stop)
+    reactor.run()
+
+if __name__ == "__main__":
+    main()
