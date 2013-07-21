b5f8ec0cfbaaa8219f1c7630b23d4169381a48ad
Vlad Dumitrescu <vlad@dumitrescu.ro>
2013-07-21 17:15:35 +0200

    no more custom libcrypto

--- full-tribler-gumby/node.sh
+++ full-tribler-gumby/node.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-export LD_PRELOAD=/home/vdumitre/venv/m2cdeps/lib/libcrypto.so.10
-
-# for libswift
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/vdumitre/libs/lib
-
-python -u node.py $@
+python node.py $@
